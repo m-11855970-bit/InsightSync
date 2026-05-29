1e92cdc8b62e18397ae73e3ff6fa02ddaf0c8840
m-11855970-bit <m-11855970@moe-dl.edu.my>
2026-05-30 03:08:36 +0800

    Add files via upload

---
+++ main.py.py
@@ -0,0 +1,487 @@
+
+skorE = skorI = 0
+skorS = skorN = 0
+skorT = skorF = 0
+skorJ = skorP = 0
+
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+1) + ":\n"
+        "At a party or banquet, you usually mingle with \n"
+        "A. Many people including strangers \n"
+        "B. A few people you know\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorE += 1
+    else:
+        skorI += 1
+        
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+2) + ":\n"
+        "You are attracted to people who are\n"
+        "A. Sensible\n"
+        "B. Imaginative\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorS += 1
+    else:
+        skorN += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+3) + ":\n"
+        "Which of the following is less acceptable\n"
+        "A. Have your (head in the clouds)\n"
+        "B. Be (in a rut)\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorS += 1
+    else:
+        skorN += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+4) + ":\n"
+        "When judging others, you are more often influenced by\n"
+        "A. Rules than by circumstances\n"
+        "B. Circumstances than by rules\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorT += 1
+    else:
+        skorF += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+5) + ":\n"
+        "You are more attracted to things\n"
+        "A. Convincing\n"
+        "B. Moving\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorT += 1
+    else:
+        skorF += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+6) + ":\n"
+        "You prefer events that start with\n"
+        "A. Punctuality\n"
+        "B. Fun\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorJ += 1
+    else:
+        skorP += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+7) + ":\n"
+        "You usually make choices\n"
+        "A. Carefully\n"
+        "B. Somewhat impulsively\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorJ += 1
+    else:
+        skorP += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+8) + ":\n"
+        "In terms of receiving information in your social group, you are a\n"
+        "A. First to receive information\n"
+        "B. Last to receive information\n"
+        "Answer: "  )
+    if jawapan == "A" or jawapan == "a":
+        skorE += 1
+    else:
+        skorI += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+9) + ":\n"
+        "Facts should be...\n"
+        "A. Facts should be facts in themselves\n"
+        "B. Facts should explain principles\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorS += 1
+    else:
+        skorN += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+10) + ":\n"
+        "Authors should write based on?\n"
+        "A. Authors should write based on facts and truth\n"
+        "B. Authors should write based on analogies and imagination\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorS += 1
+    else:
+        skorN += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+11) + ":\n"
+        "You are a person who is better recognized as?\n"
+        "A. Calm\n"
+        "B.  Friendly\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorT += 1
+    else:
+        skorF += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+12) + ":\n"
+        "You are in comfortable in making decisions related to?\n"
+        "A. Logical decisions\n"
+        "B. Moral issues\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorT += 1
+    else:
+        skorF += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+13) + ":\n"
+        "Life events should ideally take place...\n"
+        "A. planned\n"
+        "B. randomly\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorJ += 1
+    else:
+        skorP += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+14) + ":\n"
+        "You think you are more?\n"
+        "A. serious and determined\n"
+        "B. easy-going\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorJ += 1
+    else:
+        skorP += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+15) + ":\n"
+        "When with others,you usually...\n"
+        "A. start the conversation\n"
+        "B. wait for the conversation to start\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorE += 1
+    else:
+        skorI += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+16) + ":\n"
+        "You are more inclined towards...\n"
+        "A. practical\n"
+        "B. fantasy\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorS += 1
+    else:
+        skorN += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+17) + ":\n"
+        "Children usually lack effort in?\n"
+        "A. study\n"
+        "B. play\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorS += 1
+    else:
+        skorN += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+18) + ":\n"
+        "Which one of the following is more satisfactory?\n"
+        "A. discuss an issue in depth\n"
+        "B. get agreement on something\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorT += 1
+    else:
+        skorF += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+19) + ":\n"
+        "You are considered more\n"
+        "A. firm than soft\n"
+        "B. soft than firm\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorT += 1
+    else:
+        skorF += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+20) + ":\n"
+        "You are more comfortable with a job that is \n"
+        "A. Permanent\n"
+        "B. Part-time\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorJ += 1
+    else:
+        skorP += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+21) + ":\n"
+        "You prioritize things more\n"
+        "A. certainty\n"
+        "B. openness\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorJ += 1
+    else:
+        skorP += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+22) + ":\n"
+        "You prefer\n"
+        "A. many friends with less close and short-term relationships\n"
+        "B. a few friends with close and long-term relationships\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorE += 1
+    else:
+        skorI += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+23) + ":\n"
+        "You trust more\n"
+        "A. experience\n"
+        "B. gut feeling\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorS += 1
+    else:
+        skorN += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+24) + ":\n"
+        "You are more interested in\n"
+        "A. production and distribution\n"
+        "B. design and research\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorS += 1
+    else:
+        skorN += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+25) + ":\n"
+        "The most praised person is the one who has strength\n"
+        "A. clear thinking\n"
+        "B. strong feelings\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorT += 1
+    else:
+        skorF += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+26) + ":\n"
+        "In yourself, the most valued trait is\n"
+        "A. not being hesitant or hesitant\n"
+        "B. being obedient\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorT += 1
+    else:
+        skorF += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+27) + ":\n"
+        "Peacefully it is better\n"
+        "A. to determine and regulate the situation\n"
+        "B. to let the situation develop on its own\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorJ += 1
+    else:
+        skorP += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+28) + ":\n"
+        "You feel more comfortable\n"
+        "A. after making a decision\n"
+        "B. before making a decision\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorJ += 1
+    else:
+        skorP += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+29) + ":\n"
+        "When the phone rings, you\n"
+        "A. quickly answer it\n"
+        "B. hope someone else answers it\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorE += 1
+    else:
+        skorI += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+30) + ":\n"
+        "In your writing you choose things that are\n"
+        "A. literal \n"
+        "B. figurative\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorS += 1
+    else:
+        skorN += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+31) + ":\n"
+        "You are more interested in the elements of\n"
+        "A. basic and core\n"
+        "B. derivative and ancillary\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorS += 1
+    else:
+        skorN += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+32) + ":\n"
+        "Which strength is more important?\n"
+        "A. the ability to reason clearly\n"
+        "B. the ability to be compassionate\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorT += 1
+    else:
+        skorF += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+33) + ":\n"
+        "Basically you consider yourself to be\n"
+        "A. stubborn\n"
+        "B. soft-hearted\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorT += 1
+    else:
+        skorF += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+34) + ":\n"
+        "You are more inclined toward events that are\n"
+        "A. planned\n"
+        "B. unplanned\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorJ += 1
+    else:
+        skorP += 1
+for i in range(1):
+    jawapan = input(
+        "Question " + str(i+35) + ":\n"
+        "You are a more personable person\n"
+        "A.  normal and routine\n"
+        "B.  a bit awkward and irregular\n"
+        "Answer: "  )
+    
+    if jawapan == "A" or jawapan == "a":
+        skorJ += 1
+    else:
+        skorP += 1
+
+jenis = ""
+
+if skorE > skorI:
+    jenis += "E"
+else:
+    jenis += "I"
+
+if skorS > skorN:
+    jenis += "S"
+else:
+    jenis += "N"
+
+if skorT > skorF:
+    jenis += "T"
+else:
+    jenis += "F"
+
+if skorJ > skorP:
+    jenis += "J"
+else:
+    jenis += "P"
+
+if jenis == "ISTJ":
+    penerangan_asas="ISTJ:This character is like a kid who likes to make a study schedule, follow rules, and make sure all homework is done on time.\n Likes real things, not just talk."
+if jenis == "ISFJ":
+    penerangan_asas="This character is very kind, likes to take care of their friends, and makes sure everyone is comfortable and not fighting.\n Likes doing things right and neatly."
+if jenis == "INFJ":
+    penerangan_asas="This character likes to think quietly, has many great ideas in their head, and wants to help others become better.\n It's like they have a special power to know what others feel."
+if jenis == "INTJ":
+    penerangan_asas="This character is like a genius kid who likes to think of the smartest ways to solve difficult problems.\n Likes to make big plans for the future."
+if jenis == "ISTP":
+    penerangan_asas="This character is like a kid who likes playing with LEGOs, fixing bikes, and figuring out how things work.\n Likes to do things when needed, doesn't talk much."
+if jenis == "ISFP":
+    penerangan_asas="This character is like a kid who likes to draw or play music, likes beautiful things, and is very kind.\n Likes to live in the moment and do what they enjoy."
+if jenis == "INFP":
+    penerangan_asas="This character is like a kid who likes reading fantasy stories, believes in the good in everyone, and is very creative.\n Has special values deep in their heart."
+if jenis == "INTP":
+    penerangan_asas="This character is like a kid who always asks why?, likes to think about strange and great ideas.\n Is good at using their logical brain to understand the world."
+if jenis == "ESTP":
+    penerangan_asas="This character is like the most fun kid in a race or a game, acts fast, and is good at solving problems when things go wrong.\n Likes exciting stuff!"
+if jenis == "ESFP":
+    penerangan_asas="This character is like the kid who loves telling jokes, likes being the center of attention, and is good at making everyone happy.\n Likes to play and have fun."
+if jenis == "ENFP":
+    penerangan_asas="This character is like a kid who likes sharing fun new ideas, is good at making friends feel excited, and likes trying new things."
+if jenis == "ENTP":
+    penerangan_asas="This character is like a kid who likes to debate (in a good way!), likes sharing wild new ideas,\n and is good at finding different ways to do things."
+if jenis == "ESTJ":
+    penerangan_asas="This character is like a kid who likes to be the class leader, likes making rules, and makes sure everyone follows instructions.\n Is good at getting things done right and fast."
+if jenis == "ESFJ":
+    penerangan_asas="This character is like a kid who likes organizing parties or events, is good at caring for friends' feelings, and makes sure everyone gets along."
+if jenis == "ENFJ":
+    penerangan_asas="This character is like a kid who is good at speaking in front of people, can make others feel confident, and likes helping everyone become better."
+if jenis == "ENTJ":
+    penerangan_asas="This character is like a kid who can make big plans for the future, is good at telling people what to do to reach goals, and isn't afraid to make tough decisions."
+    
+
+print("\n===== RESULT =====")
+print("Your personality:", jenis)
+print("Everything you need to know:\n"+ penerangan_asas)
+
